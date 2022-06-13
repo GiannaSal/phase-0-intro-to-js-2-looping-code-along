b875ff229257f49e75a30eaab098af4ef59e0ea8
Salma Jediel <giannasal03@gmail.com>
2022-06-13 09:53:20 +0300

    commit tree 1a00c6325ea395c07a18ced7286d6c88b96aa9d7

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+// const sinon = require( 'sinon' )
+// let spy = sinon.spy( console, 'log' );
+// spy.restore();
+
+const messages = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages
+}
+function countDown(){
+    let countdn = 10;
+    while (countdn >= 0){
+        console.log(countdn--);
+    }
+
+}
+
+
+
+
